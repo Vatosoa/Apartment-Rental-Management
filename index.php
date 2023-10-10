4e9d661d5042558325583eae9111ca8603b786b3
Vatosoa <razafindrazakav@gmail.com>
2023-10-10 11:38:36 +0300

    front 0.1

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content='ID-edge'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apartment Rental Website</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">    
+</head>
+<body>
+    <style>
+    *{
+        font-family: 'Poppins', sans-serif;
+    }
+    .h-font{
+        font-family: 'Merianda', cursive;
+    }
+    </style>
+
+    <!-- navbar  -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">APARTMENT RENTAL</a>
+    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Rooms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Facilities</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+      <div class="d-flex" role="search">
+        <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+           Login
+        </button>
+        <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+           Register
+        </button>        
+        </div>
+    </div>
+  </div>
+</nav>
+
+
+<!-- Modal starts -->
+<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form>
+        <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center" id="staticBackdropLabel">
+            <i class="bi bi-person-circle fs-3 me-2"></i> User Login
+          </h5>
+          <button type="reset" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control shadow-none">
+          </div>
+          <div class="mb-4">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control shadow-none">
+          </div>
+          <div class="d-flex align-items-center justify-content-between mb-2">
+            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot password?</a>
+          </div>    
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <form>
+        <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center" id="staticBackdropLabel">
+            <i class="bi bi-person-lines-fill fs-3 me-2"></i> User Registration
+          </h5>
+          <button type="reset" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+            Note: Your details must match with your ID (National ID, Passport, Driving License, etc.)
+            that will be rquired during check-in.
+          </span>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6">
+                <label class="form-label ps-0 mb-3">Name</label>
+                <input type="text" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6">
+                <label class="form-label p-0">Email address</label>
+                <input type="email" class="form-control shadow-none">
+              </div>
+            </div>
+          </div>
+          <div class="mb-3">
+          </div>
+          <div class="mb-4">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control shadow-none">
+          </div>
+          <div class="d-flex align-items-center justify-content-between mb-2">
+            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot password?</a>
+          </div>    
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal ends  -->
+
+
+<!-- navbar ends  -->
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
