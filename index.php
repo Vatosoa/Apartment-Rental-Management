960fffe4e14153ea16a88040b9bba9eeba0d3b2a
Vatosoa <razafindrazakav@gmail.com>
2023-10-12 15:55:35 +0300

    0.2

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">    
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
 </head>
-<body>
+<body class="bg-light">
     <style>
     *{
         font-family: 'Poppins', sans-serif;
@@ -27,6 +27,17 @@
     /**Firefox */
     input[type=number]{
       -moz-appearance: textfield;
+    }
+    .custom-bg{
+      background-color: #2ec1ac;
+    }
+    .custom-bg:hover{
+      background-color: #279e8c;
+    }
+    .availability-form{
+      margin-top: -50px;
+      z-index: 2;
+      position: relative;
     }
     </style>
 
@@ -164,7 +175,6 @@
   </div>
 </div>
 
-<!-- Modal ends  -->
 
 
 <!-- navbar ends  -->
@@ -172,31 +182,76 @@
 
 
 <!-- container caroussel  -->
-<div class="container-fluid">
+<div class="container-fluid px-lg-4 mt-4">
   <div class="swiper swiper-container">
     <div class="swiper-wrapper">
       <div class="swiper-slide">
-        <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="">
+        <img src="images/carousel/1.png" alt="" class="w-100 d-block">
       </div>
       <div class="swiper-slide">
-        <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="">
+        <img src="images/carousel/2.png" alt="" class="w-100 d-block">
       </div>
       <div class="swiper-slide">
-        <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="">
+        <img src="images/carousel/3.png" alt="" class="w-100 d-block">
       </div>
       <div class="swiper-slide">
-        <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="">
+        <img src="images/carousel/4.png" alt="" class="w-100 d-block">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/carousel/5.png" alt="" class="w-100 d-block">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/carousel/6.png" alt="" class="w-100 d-block">
       </div>
     </div>
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
     <div class="swiper-pagination"></div>
   </div>
 </div>
 
 
+<!-- Check availability form  -->
+<div class="container availability-form">
+  <div class="row">
+    <div class="col-lg-12 bg-white shadow p-4 rounded">
+      <h5 class="mb-4">Check Booking Availability</h5>
+      <form>
+        <div class="row align-items-end">
+          <div class="col-lg-3 mb-3">
+            <label class="form-label" style="font-weight:500;">check-in</label>    
+            <input type="date" class="form-control shadow-none">
+          </div>
+          <div class="col-lg-3 mb-3">
+            <label class="form-label" style="font-weight:500;">check-out</label>    
+            <input type="date" class="form-control shadow-none">
+          </div>
+          <div class="col-lg-3 mb-3">
+            <label class="form-label" style="font-weight: 500;">Adult</label>
+            <select  class="form-select shadow-none">
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-lg-2 mb-3">
+            <label class="form-label" style="font-weight: 500;">Children</label>
+            <select  class="form-select shadow-none">
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-lg-1 mb-lg-3 mt-2">
+            <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
 
 
+<br><br><br>
+<br><br><br>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
@@ -206,13 +261,10 @@
       var swiper = new Swiper(".swiper-container", {
         spaceBetween: 30,
         effect: "fade",
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
+        loop: true,
+        autoplay: {
+          delay: 3500,
+          disableOnInteraction: false,
         }
       })
     </script>
