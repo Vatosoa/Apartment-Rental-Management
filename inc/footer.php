e1e05483d4b3aab080d43fb8030506e9160dcd2a
Vatosoa <razafindrazakav@gmail.com>
2023-10-12 19:52:01 +0300

    HomePage Design 0.6

---
+++ inc/footer.php
@@ -0,0 +1,37 @@
+<div class="container-fluid bg-white mt-5">
+  <div class="row">
+    <div class="col-lg-4 p-4">
+      <h3 class="h-font fw-bold fs-3 mb-2">APARTMENT RENTAL</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Dolorum adipisci eaque alias quas, 
+          doloremque iusto a accusamus aperiam itaque vitae temporibus dolor praesentium at beatae possimus porro nesciunt esse officiis.
+        </p>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h5>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+    </div>
+    <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Follow us</h5>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-twitter me-1"></i> Twitter
+        </a>
+        <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-facebook me-1"></i> Facebook
+        </a>
+        <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-instagram me-1"></i> Instagram
+        </a>
+    </div>
+  </div>
+</div>
+
+
+<h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Vatosoa Razafindrazaka</h6>
