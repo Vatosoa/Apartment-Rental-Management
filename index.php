7bd5eab0d888f5e17bfd71e63849a68354e9343c
Vatosoa <razafindrazakav@gmail.com>
2023-10-12 19:38:59 +0300

    HomePage Design

--- index.php
+++ index.php
@@ -9,31 +9,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;600;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">    
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <link rel="stylesheet" href="css/common.css">
 </head>
 <body class="bg-light">
     <style>
-    *{
-        font-family: 'Poppins', sans-serif;
-    }
-    .h-font{
-        font-family: 'Merienda', cursive;
-    }
-    /** Chrome, Safari, Edge, Opera */
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button{
-      -webkit-appearance: none;
-      margin:0;
-    }
-    /**Firefox */
-    input[type=number]{
-      -moz-appearance: textfield;
-    }
-    .custom-bg{
-      background-color: #2ec1ac;
-    }
-    .custom-bg:hover{
-      background-color: #279e8c;
-    }
     .availability-form{
       margin-top: -50px;
       z-index: 2;
@@ -182,10 +161,7 @@
   </div>
 </div>
 
-
-
 <!-- navbar ends  -->
-
 
 
 <!-- container caroussel  -->
@@ -257,41 +233,367 @@
 </div>
 
 
-
 <!-- Our Rooms  -->
 <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR ROOMS</h2>
 
 <div class="container">
   <div class="row">
-    <div class="col-lg-4 col-md-6 my-3">
 
+    <div class="col-lg-4 col-md-6 my-3">
       <div class="card border-0 shadow" style="max-width: 350px; margin:auto;">
         <img src="images/rooms/1.jpg" class="card-img-top" alt="...">
-        
         <div class="card-body">
           <h5>Simple Room Name</h5>
           <h6 class="mb-4">MGA 20.000 per night</h6>
           <div class="features mb-4">
             <h6 class="mb-1">Features</h6>
-            <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-              Note: Your details must match with your ID (National ID, Passport, Driving License, etc.)
-            that will be rquired during check-in.
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Rooms
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Bathroom
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Balcony
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              3 Sofa
             </span>
           </div>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <div class="facilities mb-4">
+            <h6 class="mb-1">Facilities</h6>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Wifi
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Television
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              AC
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Room Header
+            </span>
+          </div>
+          <div class="rating mb-4">
+            <h6 class="mb-1">Rating</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>    
+            </span>       
+          </div>
+          <div class="d-flex justify-content-evenly mb-2">
+            <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book  Now</a>
+            <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+          </div>        
         </div>
-        
       </div>
     </div>
+
+    <div class="col-lg-4 col-md-6 my-3">
+      <div class="card border-0 shadow" style="max-width: 350px; margin:auto;">
+        <img src="images/rooms/2.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5>Simple Room Name</h5>
+          <h6 class="mb-4">MGA 20.000 per night</h6>
+          <div class="features mb-4">
+            <h6 class="mb-1">Features</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Rooms
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Bathroom
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Balcony
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              3 Sofa
+            </span>
+          </div>
+          <div class="facilities mb-4">
+            <h6 class="mb-1">Facilities</h6>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Wifi
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Television
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              AC
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Room Header
+            </span>
+          </div>
+          <div class="rating mb-4">
+            <h6 class="mb-1">Rating</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>    
+            </span>       
+          </div>
+          <div class="d-flex justify-content-evenly mb-2">
+            <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book  Now</a>
+            <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+          </div>        
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 my-3">
+      <div class="card border-0 shadow" style="max-width: 350px; margin:auto;">
+        <img src="images/rooms/3.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5>Simple Room Name</h5>
+          <h6 class="mb-4">MGA 20.000 per night</h6>
+          <div class="features mb-4">
+            <h6 class="mb-1">Features</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Rooms
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Bathroom
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              2 Balcony
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              3 Sofa
+            </span>
+          </div>
+          <div class="facilities mb-4">
+            <h6 class="mb-1">Facilities</h6>
+          <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Wifi
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Television
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              AC
+            </span>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              Room Header
+            </span>
+          </div>
+          <div class="rating mb-4">
+            <h6 class="mb-1">Rating</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>       
+              <i class="bi bi-star-fill text-warning"></i>    
+            </span>       
+          </div>
+          <div class="d-flex justify-content-evenly mb-2">
+            <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book  Now</a>
+            <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+          </div>        
+        </div>
+      </div>
+    </div>
+
+
+
     <div class="col-lg-12 text-center mt-5">
       <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms >>></a>
+    </div>
+
+  </div>
+</div>
+
+
+<!-- Our Facilities  -->
+<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+
+<div class="container">
+  <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/IMG_27079.svg" width="80px" alt="">
+      <h5 class="mt-3">AC</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/IMG_41622.svg" width="80px" alt="">
+      <h5 class="mt-3">Television</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/IMG_43553.svg" width="80px" alt="">
+      <h5 class="mt-3">Wifi</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/IMG_47816.svg" width="80px" alt="">
+      <h5 class="mt-3">Massage</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/IMG_49949.svg" width="80px" alt="">
+      <h5 class="mt-3">Room Header</h5>
+    </div>
+    <div class="col-lg-12 text-center mt-5">
+      <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
     </div>
   </div>
 </div>
 
-<br><br><br>
-<br><br><br>
 
+<!-- Testimonials -->
+<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
+
+<div class="container mt-5">
+
+<!-- Swiper -->
+  <div class="swiper swiper-testimonials">
+    <div class="swiper-wrapper mb-5">
+
+      <div class="swiper-slide bg-white p-4">
+        <div class="profile d-flex align-items-center mb-3">
+          <img src="images/about/rating.svg" width="30px" alt="">
+          <h6 class="m-0 ms-2">Random user1</h6>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus cum dolor nam corrupti. 
+        </p>
+        <div class="rating">
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+        </div>
+      </div>
+
+      <div class="swiper-slide bg-white p-4">
+        <div class="profile d-flex align-items-center mb-3">
+          <img src="images/about/rating.svg" width="30px" alt="">
+          <h6 class="m-0 ms-2">Random user1</h6>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus cum dolor nam corrupti. 
+        </p>
+        <div class="rating">
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+        </div>
+      </div>
+
+      <div class="swiper-slide bg-white p-4">
+        <div class="profile d-flex align-items-center mb-3">
+          <img src="images/about/rating.svg" width="30px" alt="">
+          <h6 class="m-0 ms-2">Random user1</h6>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus cum dolor nam corrupti. 
+        </p>
+        <div class="rating">
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+          <i class="bi bi-star-fill text-warning"></i>
+        </div>
+      </div>
+
+
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+  <div class="col-lg-12 text-center mt-5">
+    <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+  </div>    
+
+</div>
+
+
+<!-- Reach us  -->
+<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+      <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d30198.573762513042!2d47.49474236922342!3d-18.894988615425714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x21f07f0712655403%3A0x285e55b835194554!2sGrand%20Appart&#39;%20Antaninarenina%2C%208%20Rue%20G%C3%A9n%C3%A9ral%20Rabehevitra%2C%20Antananarivo!3m2!1d-18.910506599999998!2d47.5237365!4m5!1s0x21f07de34f1f4eb3%3A0xdf110608bcc082f9!2sTananarive!3m2!1d-18.8791902!2d47.5079055!5e0!3m2!1sfr!2smg!4v1697124249363!5m2!1sfr!2smg"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>      
+    </div>
+
+    <div class="col-lg-4 col-md-4">
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Call us</h5>
+          <a href="tel: +261346301707" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i> +261346301707
+          </a>
+          <br>
+          <a href="tel: +261346301707" class="d-inline-block text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i> +261346301707
+          </a>
+        </div>
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Follow us</h5>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+            <i class="bi bi-twitter me-1"></i> Twitter
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+            <i class="bi bi-facebook me-1"></i> Facebook
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+            <i class="bi bi-instagram me-1"></i> Instagram
+            </span>
+          </a>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="container-fluid bg-white mt-5">
+  <div class="row">
+    <div class="col-lg-4 p-4">
+      <h3 class="h-font fw-bold fs-3 mb-2">APARTMENT RENTAL</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Dolorum adipisci eaque alias quas, 
+          doloremque iusto a accusamus aperiam itaque vitae temporibus dolor praesentium at beatae possimus porro nesciunt esse officiis.
+        </p>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h5>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact</a> <br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+    </div>
+    <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Follow us</h5>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-twitter me-1"></i> Twitter
+        </a>
+        <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-facebook me-1"></i> Facebook
+        </a>
+        <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-instagram me-1"></i> Instagram
+        </a>
+    </div>
+  </div>
+</div>
+
+
+<h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Vatosoa Razafindrazaka</h6>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
@@ -307,6 +609,39 @@
           disableOnInteraction: false,
         }
       })
+
+      var swiper = new Swiper(".swiper-testimonials", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      slidesPerView: "3",
+      loop: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        }
+      }
+    });
     </script>
 
 </body>
