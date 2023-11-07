f99f3417f43ad3c7fcb3b998b09d546760fad355
Vatosoa <razafindrazakav@gmail.com>
2023-11-07 15:32:34 +0300

    Update

--- admin/ajax/settings_crud.php
+++ admin/ajax/settings_crud.php
@@ -4,14 +4,23 @@
     require('../inc/essentials.php');
     adminLogin();
 
-    if(isset($_POST['get_general'])){
-        $q = "SELECT * FROM `settings` WHERE `sr_no=?`";
+    # READ
+    if(isset($_POST['get_general'])) {
+        $q = "SELECT * FROM `settings` WHERE `sr_no`=?";
         $values = [1];
         $res = select($q, $values, "i");
         $data = mysqli_fetch_assoc($res);
         $json_data = json_encode($data);
         echo $json_data;
-
     }
 
+    # UPDATE
+    if(isset($_POST['upd_general'])) {
+        $frm_data = filteration($_POST);
+
+        $q = "UPDATE `settings` SET `site_title`=?, `site_about`=? WHERE `sr_no`=?";
+        $values = [$frm_data['site_title'], $frm_data['site_about'], 1];
+        $res = update($q, $values, 'ssi');
+        echo $res;
+    }
 ?>
--- admin/settings.php
+++ admin/settings.php
@@ -20,7 +20,7 @@
             <div class="col-lg-10 ms-auto p-4 overflow-hidden">
                 <h3 class="mb-4">SETTINGS</h3>
 
-                <!-- General Settings Action -->
+                <!-- General Settings Section -->
                 <div class="card">
             
                     <div class="card-body">
@@ -29,12 +29,13 @@
                             <button type="button" class="btn btn-dark shadow-none btn-sù" data-bs-toggle="modal" data-bs-target="#general-s"><i class="bi bi-pencil-square"></i> Edit</button>
                         </div>
                         <h5 class="card-su btitle mb-1 fw-bold">Site Title</h5>
-                        <p class="card-text">content</p>
+                        <p class="card-text" id="site_title"></p>
                         <h5 class="card-subtitle mb-1 fw-bold">About us</h5>
-                        <p class="card-text">content</p>
+                        <p class="card-text" id="site_about"></p>
                     </div>
                 </div>
 
+                <!-- General Settings Modal -->
                 <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true">
                     <div class="modal-dialog">
                         <form>
@@ -45,16 +46,16 @@
                                 <div class="modal-body">
                                     <div class="mb-3">
                                         <label class="form-label">Site Title</label>
-                                        <input type="text" name="site_title" class="form-control shadow-none" name="site_title">
+                                        <input type="text" name="site_title" id="site_title_inp" class="form-control shadow-none">
                                     </div>
                                     <div class="mb-3">
                                         <label class="form-label">About us</label>
-                                        <textarea name="site_about" rows="6" class="form-control shadow-none"></textarea>
+                                        <textarea name="site_about" id="site_about_inp" rows="6" class="form-control shadow-none"></textarea>
                                     </div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
-                                    <button type="button" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                    <button type="button" onclick="site_title.value = general_data.site_title, site_about.value = general_data.site_about" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
+                                    <button type="button" onclick="upd_general(site_title.value, site_about.value)" class="btn custom-bg text-white shadow-none">SUBMIT</button>
                                 </div>
                             </div>
                         </form>
@@ -70,20 +71,53 @@
 <script>
     let general_data;
 
-    function get_general()
-    {
-        let site_title;
-        let site_about;
+    function get_general() {
+        let site_title = document.getElementById('site_title');
+        let site_about = document.getElementById('site_about');
+
+        let site_title_inp = document.getElementById('site_title_inp');
+        let site_about_inp = document.getElementById('site_about_inp');
+
         let xhr = new XMLHttpRequest();
         xhr.open("POST", "ajax/settings_crud.php", true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-        xhr.onload = function(){
-            general_data = this.responseText;
-            console.log(general_data);
+        xhr.onload = function() {
+            general_data = JSON.parse(this.responseText);
+            // console.log(general_data);
+            
+            site_title.innerText = general_data.site_title;
+            site_about.innerText = general_data.site_about;
+
+            site_title_inp.value = general_data.site_title;
+            site_about_inp.value = general_data.site_about;
         }
 
         xhr.send('get_general');
+    }
+
+
+    function upd_general(site_title_val, site_about_val) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "ajax/settings_crud.php", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onload = function() {
+            var myModal = document.getElementById('general-s');
+            var modal= bootstrap.Modal.getInstance(myModal);
+            modal.hide();
+
+            if(this.responseText == 1) {
+                console.log('data updated');
+                get_general();
+            }
+            else {
+                console.log("no changes made");
+            }
+        }
+
+        xhr.send('site_title='+site_title_val+'&site_about='+site_about_val+'&upd_general');         
+
     }
 
     window.onload = function(){
